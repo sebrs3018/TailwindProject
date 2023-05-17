90709b82cd786525d14e2be56b756c253336cba3
sebrs3018 <sebastian.r.s3107@hotmail.com>
2023-05-18 00:01:31 +0200

    Validation done

--- src/Views/FloatingLabelDemo.vue
+++ src/Views/FloatingLabelDemo.vue
@@ -1,8 +1,64 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+const darkMode = ref(false)
+const darkModeLabel = ref('Light')
+const emailEl = ref<HTMLInputElement | null>(null)
+const passwordEl = ref<HTMLInputElement | null>(null)
+
+const formState = reactive<{
+  email: {
+    value: string
+    selector: string
+  }
+  password: {
+    value: string
+    selector: string
+  }
+}>({
+  email: {
+    value: '',
+    selector: 'email'
+  },
+  password: {
+    value: '',
+    selector: 'password'
+  }
+})
+
+const toggleDarkMode = () => {
+  //Toggling!
+  darkMode.value = !darkMode.value
+  if (darkMode.value) {
+    document.documentElement.classList.add('dark')
+    darkModeLabel.value = 'Light'
+  } else {
+    document.documentElement.classList.remove('dark')
+    darkModeLabel.value = 'Dark'
+  }
+}
+const submit = () => {
+  for (const e of [formState.email, formState.password]) {
+    const { value, selector } = e
+    const el = document.getElementById(selector) as HTMLInputElement
+    console.log({ el, value })
+    if (!value.trim()) {
+      el.setCustomValidity('nope')
+      return
+    }
+    el.setCustomValidity('')
+  }
+}
+</script>
 <template>
-  <div class="flex h-full w-full items-center justify-center">
-    <div class="flex h-[485px] w-[592px] flex-col items-center p-5">
-      <picture
+  <button
+    class="absolute top-3 left-3 rounded-xl border border-gray-400 py-1 px-8 dark:text-white"
+    @click.stop="toggleDarkMode"
+  >
+    {{ darkModeLabel }}
+  </button>
+  <div class="flex h-full w-full items-center justify-center dark:bg-rich-black">
+    <div class="flex h-[485px] w-[592px] flex-col items-center rounded-2xl border p-5 shadow-lg">
+      <picture class="dark:brightness-0 dark:invert"
         ><source
           media="(max-width: 567px)"
           srcset="https://cdn.wk-cdn.it/2023.6.5/_ui/img/WikiLogo/Logo-wikicasa.svg"
@@ -27,22 +83,48 @@
           alt="Google"
           class="inline-block"
         />
-        <span class="ml-2">Accedi con Google</span>
+        <span class="ml-2 dark:text-white">Accedi con Google</span>
       </button>
       <p
-        class="relative w-full text-center font-semibold before:absolute before:left-0 before:top-3 before:h-[1px] before:w-[40%] before:bg-slate-300 after:absolute after:right-0 after:top-3 after:h-[1px] after:w-[40%] after:bg-slate-300"
+        class="relative w-full text-center font-semibold before:absolute before:left-0 before:top-3 before:h-[1px] before:w-[40%] before:bg-slate-300 after:absolute after:right-0 after:top-3 after:h-[1px] after:w-[40%] after:bg-slate-300 dark:text-white"
       >
         oppure
       </p>
-      <form class="w-full justify-center">
+      <form class="w-full justify-center" @submit.prevent.stop="submit">
         <div class="relative mt-3">
-          <input id="email" type="email" placeholder="Email" class="w-full" />
+          <input
+            ref="emailEl"
+            id="email"
+            type="email"
+            v-model="formState.email.value"
+            @input="emailEl?.setCustomValidity('')"
+            placeholder="mock"
+            class="peer h-10 w-full border-b-2 border-gray-300 text-eerie-black placeholder-transparent invalid:border-fleery-rose focus:border-w-blue focus:outline-none focus:invalid:border-fleery-rose dark:bg-rich-black dark:text-white"
+          />
+          <label
+            for="email"
+            class="pointer-events-none absolute -top-3.5 left-0 text-sm text-gray-900 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-900 dark:text-gray-200 dark:peer-focus:text-gray-200"
+            >Email</label
+          >
         </div>
         <div class="relative mt-3">
-          <input id="password" type="password" placeholder="Password" class="w-full" />
+          <input
+            ref="passwordEl"
+            id="password"
+            type="password"
+            v-model="formState.password.value"
+            @input="passwordEl?.setCustomValidity('')"
+            placeholder="Password"
+            class="h-10 w-full border-b-2 border-gray-300 text-eerie-black focus:border-w-blue focus:outline-none"
+          />
         </div>
         <p class="my-5 text-sm text-w-blue">Password dimenticata?</p>
-        <button class="h-10 w-full rounded-3xl bg-w-blue text-white" type="submit">Accedi</button>
+        <button
+          class="h-10 w-full rounded-3xl bg-w-blue font-semibold text-white dark:bg-white dark:text-w-blue"
+          type="submit"
+        >
+          Accedi
+        </button>
       </form>
     </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/App.vue', './src/**/*.vue'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
         'w-blue': '#2b5dff',
         periwinkle: '#b4c2f0',
         'eerie-black': '#222',
+        'rich-black': '#18202c',
+        'fleery-rose': '#fa4f64'
       }
     }
   },
